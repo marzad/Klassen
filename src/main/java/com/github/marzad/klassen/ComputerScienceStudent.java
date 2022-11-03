3285310e4966b8933d20794604b84341db1de604
marzad <maschatz@gmx.net>
2022-11-03 11:16:03 +0100

    Update ComputerScienceStudent

--- src/main/java/com/github/marzad/klassen/ComputerScienceStudent.java
+++ src/main/java/com/github/marzad/klassen/ComputerScienceStudent.java
@@ -5,11 +5,24 @@ import com.github.marzad.klassen.Student;
 public class ComputerScienceStudent extends Student {
     String adress;
 
+    public ComputerScienceStudent() {
+    }
+
+    public ComputerScienceStudent(String fname, String lname, long id, String bday, String course, String adress) {
+        super(fname, lname, id, bday, course);
+        this.adress = adress;
+    }
+
     public String getAdress() {
         return adress;
     }
 
     public void setAdress(String adress) {
         this.adress = adress;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " und wohnt in " + this.adress;
     }
 }
